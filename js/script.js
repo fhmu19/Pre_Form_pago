f7eb8c091eb5872914dd5a168a0c016bbf615a6d
Fernando <fhmu19@gmail.com>
2021-02-12 17:55:05 -0500

    Version 1 Formulario de pagos y formulario de registro

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function verifySpaces(){
+    const name=document.getElementById('name').value
+    const lastName=document.getElementById('lastName').value
+    const numCard=document.getElementById('numCard').value
+    const endDate=document.getElementById('endDate').value
+    const cvvCode=document.getElementById('cvvCode').value
+    const array =[name, lastName, numCard, endDate, cvvCode]
+}
